949b40b5fe1c1d7d92459b0637e5e35387416d0b
김진수 <kimjinsu@kimjinsuui-MacBookAir.local>
2022-06-13 02:38:33 +0900

    second commit

--- index.ts
+++ index.ts
@@ -1,5 +1,24 @@
 import { argv } from 'yargs';
+import { Kafka } from 'kafkajs';
 
 (async () => {
   console.info(JSON.stringify(argv));
+
+  const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['127.0.0.1:9092'],
+  });
+
+  const consumer = kafka.consumer({ groupId: 'crawling-group' })
+
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'crawling-events', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      })
+    },
+  })
 })();
